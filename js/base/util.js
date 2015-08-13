046ca1a53884a9d3a57203e377a6200927ba5711
ljwrer <ljwrer@sina.com>
2015-08-14 00:02:00 +0800

    login and follow

--- js/base/util.js
+++ js/base/util.js
@@ -61,16 +61,17 @@ define(["base"], function(base) {
 				var result = new Array();
 				var nodes = element.getElementsByTagName("*");
 				var namesArray = names.split(' ');
+				var hasAllClassName = [];
 				for (var j = 0, len = nodes.length; j < len; j++) {
-					nodes[j].hasAllClassName = true;
+					hasAllClassName[j] = true;
 					for (var i = 0; i < namesArray.length; i++) {
 						if (nodes[j].className.indexOf(namesArray[i]) === -1) {
-							nodes[j].hasAllClassName = false;
+							hasAllClassName[j] = false;
 						}
 					}
 				}
 				for (i = 0; i < len; i++) {
-					if (nodes[i].hasAllClassName) {
+					if (hasAllClassName[i]) {
 						result[result.length] = nodes[i];
 					}
 				}
@@ -88,23 +89,117 @@ define(["base"], function(base) {
 			elem.removeEventListener(type, listener, useCapture);
 		} : function(elem, type, listener, useCapture) {
 			elem.detachEvent('on' + type, listener);
-		}
-	}
-	var StyleUtil={
-		hide:function(ele){
-			DomUtil.removeClass(ele,"j-show");
-			DomUtil.addClass(ele,"j-hide");
 		},
-		show:function(ele){
-			DomUtil.removeClass(ele,"j-hide");
-			DomUtil.addClass(ele,"j-show");
+		preventDefault: function(event) {
+			if (event.preventDefault) {
+				event.preventDefault();
+			} else {
+				event.returnValue = false;
+			}
+		},
+		getEvent: function(event) {
+			return event ? event : window.event;
+		},
+		getTarget: function(event) {
+			return event.target || event.srcElement;
 		}
-	}
+	};
+	var StyleUtil = {
+		hide: function(ele) {
+			DomUtil.removeClass(ele, "j-show");
+			DomUtil.addClass(ele, "j-hide");
+		},
+		show: function(ele) {
+			DomUtil.removeClass(ele, "j-hide");
+			DomUtil.addClass(ele, "j-show");
+		}
+	};
+
+	var FormUtil = {
+		resetFields: function(whichform) {
+			if ('placeholder' in document.createElement('input')) return;
+			for (var i = 0; i < whichform.elements.length; i++) {
+				var element = whichform.elements[i];
+				if (element.type == "submit") continue;
+				if (!element.getAttribute('placeholder')) continue;
+				element.onfocus = function() {
+					if (this.value == this.getAttribute('placeholder')) {
+						this.value = "";
+					}
+				}
+				element.onblur = function() {
+					if (this.value == "") {
+						this.value = this.getAttribute('placeholder');
+					}
+				}
+				element.onblur();
+			}
+		}
+	};
+	var AjaxUtil = {
+		getHTTPObject: function() {
+			if (typeof XMLHttpRequest === undefined) {
+				try {
+					return new ActiveXObject("Msxml2.XMLHTTP.6.0");
+				} catch (e) {}
+				try {
+					return new ActiveXObject("Msxml2.XMLHTTP.3.0");
+				} catch (e) {}
+				try {
+					return new ActiveXObject();
+				} catch (e) {}
+			}
+			return new XMLHttpRequest();
+		},
+		serialize: function(data) {
+			var ret = [];
+			for (var name in data) {
+				var value = data[name];
+				if (!data.hasOwnProperty(name)) continue;
+				if (typeof data[name] == "function") continue;
+				name = encodeURIComponent(name);
+				value = encodeURIComponent(value);
+				ret.push(name + "=" + value);
+			}
+			ret = ret.join("&");
+			return ret;
+		},
+		getAjax: function(url, options, callback) {
+			var xhr = new this.getHTTPObject();
+			xhr.open("GET", url + "?" + this.serialize(options), true);
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == 4) {
+					if (xhr.status >= 200 && xhr.status <= 300 || xhr.status == 304) {
+						callback(xhr.responseText);
+					} else {
+//						alert("get failed,status:" + xhr.status);
+					}
+				}
+			};
+			xhr.send(null);
+		},
+		postAjax: function(url, options, callback) {
+			var xhr = new this.getHTTPObject();
+			xhr.open("POST", url, true);
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == 4) {
+					if (xhr.status >= 200 && xhr.status <= 300 || xhr.status == 304) {
+						callback(xhr.responseText);
+					} else {
+//						alert("post failed,status:" + xhr.status);
+					}
+				}
+			};
+			xhr.send(this.serialize(options));
+		}
+	};
 
 	return {
 		CookieUtil: CookieUtil,
 		DomUtil: DomUtil,
-		EventUtil:EventUtil,
-		StyleUtil:StyleUtil
+		EventUtil: EventUtil,
+		StyleUtil: StyleUtil,
+		FormUtil: FormUtil,
+		AjaxUtil: AjaxUtil
 	};
 })
--- js/main/main.js
+++ js/main/main.js
@@ -2,11 +2,16 @@ require.config({
 	baseUrl: "js",
 	paths: {
 		"base": "base/base",
-		"util": "base/util"
+		"util": "base/util",
+		"md5": "lib/md5.min"
+	},
+	shim: {　　　　　　
+		'md5': {　　　　　　　　
+			exports: 'md5'　　　　
+		}
 	}
 })
-require(["base", "util"], function(base, util) {
-
+require(["base", "util", "md5"], function(base, util, md5) {
 	/* 关闭顶部通知条 */
 	var gtip = util.DomUtil.getElementsByClassName("g-tip")[0];
 	var ghead = util.DomUtil.getElementsByClassName("g-head")[0];
@@ -15,8 +20,7 @@ require(["base", "util"], function(base, util) {
 		gtip.className = "g-tip";
 		gtip.innerHTML = "";
 		var htmlText = "";
-		htmlText += '<div class="m-tip clear-fix">' + '<p>网易云课堂微专业，帮助你掌握专业技能，令你求职或加薪多一份独特优势！<a href="http://study.163.com/smartSpec/intro.htm" target="_blank">立即查看></a></p>' + '<div class="u-notip" id="notip">'
-		+ '<p class="u-sprite"></p>' + '<span>不再提醒</span>' + '</div>';
+		htmlText += '<div class="m-tip clear-fix">' + '<p>网易云课堂微专业，帮助你掌握专业技能，令你求职或加薪多一份独特优势！<a href="http://study.163.com/smartSpec/intro.htm" target="_blank">立即查看></a></p>' + '<div class="u-notip" id="notip">' + '<p class="u-sprite"></p>' + '<span>不再提醒</span>' + '</div>';
 		gtip.innerHTML = htmlText;
 		ghead.parentNode.insertBefore(gtip, ghead);
 		var notip = document.getElementById("notip");
@@ -25,20 +29,56 @@ require(["base", "util"], function(base, util) {
 			document.body.removeChild(gtip);
 		}
 	}
-	
-	/* 关注“网易教育产品部” /登录 */
-	var follow=document.getElementById("follow");
-	var login=document.getElementById("login");
-	util.EventUtil.addEvent(follow,"click",followHandle,false);
-	function followHandle(){
-		if(!util.CookieUtil.get("loginSuc"))
-		{
+
+	/* 关注网易教育产品部 登录  */
+	var follow = document.getElementById("follow");
+	var unfollow = document.getElementById("unfollow");
+	var login = document.getElementById("login");
+	util.EventUtil.addEvent(follow, "click", followHandle);
+	util.EventUtil.addEvent(unfollow, "click", unfollowHandle);
+	function followHandle() {
+		if (!util.CookieUtil.get("loginSuc")) {
 			util.StyleUtil.show(login);
-			var closeLogin=document.getElementById("closeLogin");
-			closeLogin.onclick=function(){
+			var loginForm = login.getElementsByTagName("form")[0];
+			util.FormUtil.resetFields(loginForm);
+			var closeLogin = document.getElementById("closeLogin");
+			closeLogin.onclick = function() {
 				util.StyleUtil.hide(login);
+			};
+			util.EventUtil.addEvent(loginForm, "submit", submitHandle);
+			function submitHandle(event) {
+				event = util.EventUtil.getEvent(event);
+				util.EventUtil.preventDefault(event);
+				var target = util.EventUtil.getTarget(event);
+				var btn = target.elements[2];
+				btn.disabled = true;
+				var option = {};
+				option.userName = md5(target.elements[0].value);
+				option.password = md5(target.elements[1].value);
+				util.AjaxUtil.getAjax("http://study.163.com/webDev/login.htm", option, loginHandle);
+				function loginHandle(back) {
+					if (back == 1) {
+						util.StyleUtil.hide(login);
+						util.StyleUtil.hide(follow.parentNode);
+						util.StyleUtil.show(unfollow.parentNode);
+						util.CookieUtil.set("followSuc", true);
+						util.CookieUtil.set("loginSuc", true);
+					} else {
+						btn.disabled = false;
+					}
+				}
 			}
-		}	
+		} else {
+			util.CookieUtil.set("followSuc", true);
+			util.StyleUtil.hide(follow.parentNode);
+			util.StyleUtil.show(unfollow.parentNode);
+		}
 	}
-	
+	function unfollowHandle() {
+		event = util.EventUtil.getEvent(event);
+		var target = util.EventUtil.getTarget(event);
+		util.StyleUtil.hide(unfollow.parentNode);
+		util.StyleUtil.show(follow.parentNode);
+		util.CookieUtil.set("followSuc", false);
+	}
 })
