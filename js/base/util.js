8f3df43f7fd48598b9ef6ae390863c58ee26b22d
ljwrer <ljwrer@sina.com>
2015-08-13 17:34:02 +0800

    tip finish

--- js/base/util.js
+++ js/base/util.js
@@ -1,13 +1,110 @@
-define([],function(){
-	function getCookie(){
-		
+define(["base"], function(base) {
+	if (!("innerText" in document.body)) {
+		HTMLElement.prototype.__defineGetter__("innerText", function() {
+			return this.textContent;
+		});
+		HTMLElement.prototype.__defineSetter__("innnerText", function(s) {
+			return this.textContent = s;
+		});
 	}
-	
-	function setCookie(name,value){
-		
+	var CookieUtil = {
+		get: function(name) {
+			if (!document.cookie) return null;
+			var allCookie = document.cookie;
+			var CookieName = encodeURIComponent(name) + "=",
+				CookieValue = null,
+				CookieStart = allCookie.indexOf(CookieName),
+				CookieEnd;
+			if (CookieStart > -1) {
+				CookieEnd = allCookie.indexOf(";", CookieStart);
+				if (CookieEnd == -1) {
+					CookieEnd = allCookie.length;
+				}
+				CookieValue = decodeURIComponent(allCookie.substring(CookieStart + CookieName.length, CookieEnd));
+			}
+			return CookieValue;
+		},
+		set: function(name, value, expires, domain, path, secure) {
+			var CookieText = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+			if (expires instanceof Date) {
+				CookieText += " ;expires=" + expires.toGMTString();
+			}
+			if (domain) {
+				CookieText += " ;domain=" + domain;
+			}
+			if (path) {
+				CookieText += " ;path" + path;
+			}
+			if (secure) {
+				CookieText += " ;secure" + secure;
+			}
+			document.cookie = CookieText;
+		}
+	};
+	var DomUtil = {
+		hasClass: function(ele, cls) {
+			return (new RegExp('(\\s|^)' + cls + '(\\s|$)')).test(ele.className);
+		},
+		addClass: function(ele, cls) {
+			if (!this.hasClass(ele, cls)) ele.className += " " + cls;
+		},
+		removeClass: function(ele, cls) {
+			if (this.hasClass(ele, cls)) {
+				ele.className = ele.className.replace(new RegExp('(\\s|^)' + cls + '(\\s|$)'), " ");
+			}
+		},
+		getElementsByClassName: function(names, element) {
+			if (!element) element = document;
+			if (document.getElementsByClassName) {
+				return element.getElementsByClassName(names);
+			} else {
+				var result = new Array();
+				var nodes = element.getElementsByTagName("*");
+				var namesArray = names.split(' ');
+				for (var j = 0, len = nodes.length; j < len; j++) {
+					nodes[j].hasAllClassName = true;
+					for (var i = 0; i < namesArray.length; i++) {
+						if (nodes[j].className.indexOf(namesArray[i]) === -1) {
+							nodes[j].hasAllClassName = false;
+						}
+					}
+				}
+				for (i = 0; i < len; i++) {
+					if (nodes[i].hasAllClassName) {
+						result[result.length] = nodes[i];
+					}
+				}
+				return result;
+			}
+		}
+	};
+	var EventUtil = {
+		addEvent: document.addEventListener ? function(elem, type, listener, useCapture) {
+			elem.addEventListener(type, listener, useCapture);
+		} : function(elem, type, listener, useCapture) {
+			elem.attachEvent('on' + type, listener);
+		},
+		delEvent: document.removeEventListener ? function(elem, type, listener, useCapture) {
+			elem.removeEventListener(type, listener, useCapture);
+		} : function(elem, type, listener, useCapture) {
+			elem.detachEvent('on' + type, listener);
+		}
 	}
-	
+	var StyleUtil={
+		hide:function(ele){
+			DomUtil.removeClass(ele,"j-show");
+			DomUtil.addClass(ele,"j-hide");
+		},
+		show:function(ele){
+			DomUtil.removeClass(ele,"j-hide");
+			DomUtil.addClass(ele,"j-show");
+		}
+	}
+
 	return {
-		
+		CookieUtil: CookieUtil,
+		DomUtil: DomUtil,
+		EventUtil:EventUtil,
+		StyleUtil:StyleUtil
 	};
 })
--- js/main/main.js
+++ js/main/main.js
@@ -1,14 +1,44 @@
 require.config({
-	baseUrl:"js",
-	paths:{
-		"base":"base/base"
-		"util":"base/util"
+	baseUrl: "js",
+	paths: {
+		"base": "base/base",
+		"util": "base/util"
 	}
 })
-require(["base","util"],function(base,util){
-	/* 不再提醒 */
-	var notip=document.getElementById("notip");
-	notip.onclick=function(){
-		
+require(["base", "util"], function(base, util) {
+
+	/* 关闭顶部通知条 */
+	var gtip = util.DomUtil.getElementsByClassName("g-tip")[0];
+	var ghead = util.DomUtil.getElementsByClassName("g-head")[0];
+	if (!util.CookieUtil.get("notip")) {
+		var gtip = document.createElement("div");
+		gtip.className = "g-tip";
+		gtip.innerHTML = "";
+		var htmlText = "";
+		htmlText += '<div class="m-tip clear-fix">' + '<p>网易云课堂微专业，帮助你掌握专业技能，令你求职或加薪多一份独特优势！<a href="http://study.163.com/smartSpec/intro.htm" target="_blank">立即查看></a></p>' + '<div class="u-notip" id="notip">'
+		+ '<p class="u-sprite"></p>' + '<span>不再提醒</span>' + '</div>';
+		gtip.innerHTML = htmlText;
+		ghead.parentNode.insertBefore(gtip, ghead);
+		var notip = document.getElementById("notip");
+		notip.onclick = function() {
+			util.CookieUtil.set("notip", true);
+			document.body.removeChild(gtip);
+		}
 	}
+	
+	/* 关注“网易教育产品部” /登录 */
+	var follow=document.getElementById("follow");
+	var login=document.getElementById("login");
+	util.EventUtil.addEvent(follow,"click",followHandle,false);
+	function followHandle(){
+		if(!util.CookieUtil.get("loginSuc"))
+		{
+			util.StyleUtil.show(login);
+			var closeLogin=document.getElementById("closeLogin");
+			closeLogin.onclick=function(){
+				util.StyleUtil.hide(login);
+			}
+		}	
+	}
+	
 })
